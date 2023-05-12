43b3f43257d7d8eccf693132143cdad3c862225a
liz-fw <lstebbins@flowwest.com>
2023-05-12 16:45:50 -0700

    updates saved objects for STAN model

--- data-raw/adult_model/predicted_spawners_STAN.R
+++ data-raw/adult_model/predicted_spawners_STAN.R
@@ -85,6 +85,8 @@ predicted_redds <- "
     }
 }"
 
+
+# battle ------------------------------------------------------------------
 full_battle <- full_data_for_input |>
   filter(stream == "battle creek") |>
   drop_na(wy_type_std) |>
@@ -99,18 +101,111 @@ battle_pred_redd <- stan(model_code = predicted_redds,
                        data = battle_data_list,
                        chains = 3, iter = 20000*2, seed = 84735)
 
-loo(battle_pred_redd)
+
+# clear -------------------------------------------------------------------
+full_clear <- full_data_for_input |>
+  filter(stream == "clear creek") |>
+  drop_na(max_flow_std) |>
+  glimpse()
+
+clear_data_list <- list("N" = length(unique(full_clear$year)),
+                         "observed_passage" = full_clear$upstream_count,
+                         "observed_spawners" = full_clear$redd_count,
+                         "environmental_covar" = as.vector(scale(full_clear$max_flow_std)))
+
+clear_pred_redd <- stan(model_code = predicted_redds,
+                         data = clear_data_list,
+                         chains = 3, iter = 20000*2, seed = 84735)
+
+
+# mill --------------------------------------------------------------------
+full_mill <- full_data_for_input |>
+  filter(stream == "mill creek") |>
+  drop_na(gdd_std) |>
+  glimpse()
+
+mill_data_list <- list("N" = length(unique(full_mill$year)),
+                         "observed_passage" = full_mill$upstream_count,
+                         "observed_spawners" = full_mill$redd_count,
+                         "environmental_covar" = as.vector(scale(full_mill$gdd_std)))
+
+mill_pred_redd <- stan(model_code = predicted_redds,
+                         data = mill_data_list,
+                         chains = 3, iter = 20000*2, seed = 84735)
+
+
+# deer --------------------------------------------------------------------
+full_deer <- full_data_for_input |>
+  filter(stream == "deer creek") |>
+  drop_na(wy_type_std) |>
+  glimpse()
+
+deer_data_list <- list("N" = length(unique(full_deer$year)),
+                         "observed_passage" = full_deer$upstream_count,
+                         "observed_spawners" = full_deer$holding_count,
+                         "environmental_covar" = as.vector(scale(full_deer$wy_type_std)))
+
+deer_pred_redd <- stan(model_code = predicted_redds,
+                         data = deer_data_list,
+                         chains = 3, iter = 20000*2, seed = 84735)
+
+
+
+# get results -------------------------------------------------------------
+# define function to pull out predicted spawners
+get_predicted_spawners <- function(model_fit) {
+  par_results <- summary(model_fit)$summary
+  results_tibble <- as.data.frame(par_results) |>
+    rownames_to_column("par_names") |>
+    filter(str_detect(par_names, "predicted_spawners"))
+
+  return(results_tibble)
+}
+
+# save as object ----------------------------------------------------------
+model_fit_summaries <- list("battle_pred" = get_predicted_spawners(battle_pred_redd),
+                            "clear_pred" = get_predicted_spawners(clear_pred_redd),
+                            "mill_pred" = get_predicted_spawners(mill_pred_redd),
+                            "deer_pred" = get_predicted_spawners(deer_pred_redd))
+
+save(model_fit_summaries, file = here::here("data-raw", "adult_model",
+                                       "model_fit_summaries.Rdata"))
+
+pred_redd_fits <- list("battle_pred_redd" = battle_pred_redd,
+                       "clear_pred_redd" = clear_pred_redd,
+                       "mill_pred_redd" = mill_pred_redd,
+                       "deer_pred_redd" = deer_pred_redd)
+
+save(pred_redd_fits, file = here::here("data-raw", "adult_model",
+                                       "pred_redd_fits.Rdata"))
+
+
+# function to get par name ------------------------------------------------
+get_par_names <- function(data_list) {
+  N <- data_list$N
+
+  names <- c()
+  for(i in 1:N) {
+    names[i] <- paste0("predicted_spawners[", i, "]")
+  }
+  #names[N+1] <- "b1_survival"
+  return(names)
+}
+
+get_par_names(battle_data_list)
+
+# diagnostics -------------------------------------------------------------
 
 summary(battle_pred_redd)$summary[, 1:4]
 
 summary(battle_pred_redd)$summary[str_detect(rownames(summary(battle_pred_redd)$summary), "predicted_spawners"), 1:4] |> round(4)
 summary(battle_pred_redd)$summary[rownames(summary(battle_pred_redd)$summary) == "b1_survival", 1:10] |> round(4)
 
-mcmc_areas(battle_pred_redd, pars = c("b1_survival"))
-mcmc_dens_overlay(battle_pred_redd, pars = c("redds_per_spawner[15]"))
-mcmc_areas(battle_pred_redd, pars = c("mu_k", "sigma_k", "mu_a", "sigma_a"))
-mcmc_dens_overlay(battle_pred_redd, pars = c("mu_k", "sigma_k",  "mu_a", "sigma_a")) # should be indistinguishable
-neff_ratio(battle_pred_redd, pars = c("mu_k", "sigma_k", "mu_a", "sigma_a")) # should be >0.1
-mcmc_acf(battle_pred_redd, pars = c("mu_k", "sigma_k", "mu_a", "sigma_a")) # should drop to be low
-rhat(battle_pred_redd, c("mu_k", "sigma_k", "mu_a", "sigma_a")) # should be close to 1
+mcmc_trace(battle_pred_redd, pars = get_par_names(battle_data_list))
+mcmc_dens_overlay(battle_pred_redd, pars = get_par_names(battle_data_list))
+mcmc_dens_overlay(battle_pred_redd, pars = "b1_survival")
+mcmc_areas(battle_pred_redd, pars = get_par_names(battle_data_list))
+neff_ratio(battle_pred_redd, pars = get_par_names(battle_data_list)) # should be >0.1
+mcmc_acf(battle_pred_redd, pars = get_par_names(battle_data_list)) # should drop to be low
+rhat(battle_pred_redd, get_par_names(battle_data_list)) # should be close to 1
 
