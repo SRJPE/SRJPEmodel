4605d1a58330262f102e1a373ffddb3197ffa95a
liz-fw <lstebbins@flowwest.com>
2023-06-08 13:58:30 -0700

    recreates diagnostic plot

--- data-raw/adult_model/adult_model_diagnostics.R
+++ data-raw/adult_model/adult_model_diagnostics.R
@@ -63,12 +63,12 @@ diagnostic_pars <- read.csv(here::here("data-raw", "adult_model", "adult_model_d
 
 diagnostics <- report_pars |>
   filter(par_names != "b1_survival") |>
-  select(par_names, mean, sd, stream) |>
+  select(par_names, mean, X2.5., X97.5., sd, stream) |>
   glimpse()
 
 obsv_data <- full_data_for_input |>
   mutate(obsv_spawner_count = ifelse(stream == "deer creek", holding_count, redd_count)) |>
-  select(year, stream, obsv_spawner_count) |>
+  select(year, stream, obsv_spawner_count, obsv_upstream = upstream_estimate) |>
   glimpse()
 
 years_to_join <- c(full_data_for_input |> filter(stream == "battle creek") |> drop_na(upstream_estimate, redd_count, wy_type) |> pull(year),
@@ -77,7 +77,7 @@ years_to_join <- c(full_data_for_input |> filter(stream == "battle creek") |> dr
                    full_data_for_input |> filter(stream == "mill creek") |> drop_na(upstream_estimate, redd_count, gdd_std) |> pull(year)) |>
   glimpse()
 
-diagnostics_with_year <- diagnostics |>
+pred_with_year <- diagnostics |>
   arrange(stream) |>
   mutate(year = years_to_join) |>
   select(-par_names) |>
@@ -85,13 +85,13 @@ diagnostics_with_year <- diagnostics |>
   left_join(obsv_data, by = c("year", "stream")) |>
   glimpse()
 
-diagnostics_with_year |> ggplot(aes(x = obsv_spawner_count, y = pred_spawner_count)) +
+pred_with_year |> ggplot(aes(x = obsv_spawner_count, y = pred_spawner_count)) +
   geom_point() + geom_smooth(method = "lm") + facet_wrap(~stream, scales = "free") +
   theme_minimal() + xlab("Observed Spawner Count") + ylab("Predicted Spawner Count") +
   ggtitle("Predicted vs. Observed Spawner Count")
 
 # look at R2
-summary(lm(pred_spawner_count ~ obsv_spawner_count, data = diagnostics_with_year))
+summary(lm(pred_spawner_count ~ obsv_spawner_count, data = pred_with_year))
 
 # look at estimated value of sigma redds_per_spawner (mean and sd)
 diagnostic_pars |>
@@ -113,3 +113,27 @@ diagnostic_pars |>
 # plot year-specific random effects (log_redds_per_spawner[y])
 # a big portion of the variation in predicted survival_rate[y] comes from b1_survival * environmental_covar[y]
 # and not random effects
+
+rps <- diagnostic_pars |>
+  filter(str_detect(par_names, "log_mean_redds_per_spawner")) |>
+  mutate(rps = exp(mean),
+         lcl = exp(X2.5.),
+         ucl = exp(X97.5.))
+
+obsv_data |>
+  filter(!stream %in% c("feather river", "butte creek", "yuba river")) |>
+  drop_na(obsv_upstream) |>
+  left_join(rps |>
+              select(stream, rps, lcl, ucl), by = c("stream")) |>
+  ggplot(aes(x = obsv_upstream, y = obsv_spawner_count)) +
+  geom_point() +
+  geom_abline(intercept = 0, slope = 1, linetype = "dashed") +
+  geom_abline(aes(intercept = 0, slope = rps)) +
+  geom_ribbon(aes(ymin = (0 + obsv_upstream * lcl),
+                  ymax = (0 + obsv_upstream * ucl)),
+              fill = "grey70", alpha = 0.2) +
+  facet_wrap(~stream, scale = "free") +
+  theme_minimal() +
+  xlab("Observed upstream passage") +
+  ylab("Observed spawner count (redd or holding)") +
+  ggtitle("Model diagnostics")
