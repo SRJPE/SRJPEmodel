e279d9f53b3f27628f4ba30a7807186bcd3d59a9
liz-fw <lstebbins@flowwest.com>
2023-11-03 10:00:10 -0700

    creates wide and long stock-recruit tables

--- data-raw/adult_model/adult_data_table_for_spawn_recruit.R
+++ data-raw/adult_model/adult_data_table_for_spawn_recruit.R
@@ -49,7 +49,7 @@ P2S_model_fits_with_year <- P2S_model_fits |>
   select(-c(year_index)) |>
   relocate(year, .before = P2S_median_count) |>
   pivot_longer(c(P2S_median_count, P2S_lcl, P2S_ucl),
-               names_to = "data_type", values_to = "count") |>
+               names_to = "data_type", values_to = "count") |> # TODO to make it long, do it here
   glimpse()
 
 # bind together the two groups of data -------------------------------------
@@ -61,18 +61,44 @@ table_for_spawn_recruit <- bind_rows(P2S_model_fits_with_year,
   pivot_wider(id_cols = c(year, stream),
               names_from = data_type,
               values_from = count) |>
+  mutate(lcl = case_when(!is.na(P2S_lcl) & !is.na(P2S_median_count) ~ P2S_lcl,
+                         !is.na(carcass_90_lcl) & !is.na(carcass_estimate) ~ carcass_90_lcl,
+                         TRUE ~ NA),
+         ucl = case_when(!is.na(P2S_ucl) & !is.na(P2S_median_count) ~ P2S_ucl,
+                         !is.na(carcass_90_ucl) & !is.na(carcass_estimate) ~ carcass_90_ucl,
+                         TRUE ~ NA)) |>
+  select(-c(P2S_lcl, P2S_ucl, carcass_90_lcl, carcass_90_ucl)) |>
   arrange(stream, year) |>
   glimpse()
+
+table_for_spawn_recruit_long <- table_for_spawn_recruit |>
+  mutate(cl_type = case_when(!is.na(lcl | ucl) & !is.na(P2S_median_count) ~ "P2S_modeled_count",
+                             !is.na(lcl | ucl) & !is.na(carcass_estimate) ~ "carcass_estimate",
+                             TRUE ~ NA)) |>
+  rename(P2S_modeled_count = P2S_median_count) |>
+  pivot_longer(c(P2S_modeled_count, upstream_estimate, redd_count, holding_count,
+                 carcass_estimate),
+               names_to = "data_type", values_to = "count") |>
+  filter(!is.na(count)) |>
+  mutate(lcl = ifelse(cl_type != data_type, NA, lcl),
+         ucl = ifelse(cl_type != data_type, NA, ucl)) |>
+  relocate(c(data_type, count), .before = lcl) |>
+    select(-c(cl_type)) |>
+  glimpse()
+
 
 # upload to google cloud --------------------------------------------------
 
 f <- function(input, output) write_csv(input, file = output)
 
-gcs_upload(table_for_spawn_recruit,
+gcs_upload(table_for_spawn_recruit_long,
            object_function = f,
            type = "csv",
            name = "jpe-model-data/adult-model/adult_data_for_spawn_recruit.csv")
 
-write.csv(table_for_spawn_recruit, here::here("data-raw", "adult_model", "adult_model_data",
+write.csv(table_for_spawn_recruit_long, here::here("data-raw", "adult_model", "adult_model_data",
                                              "adult_data_for_spawn_recruit.csv"),
           row.names = FALSE)
+write.csv(table_for_spawn_recruit, here::here("data-raw", "adult_model", "adult_model_data",
+                                              "table_for_spawn_recruit_wide.csv"),
+          row.names = F)
