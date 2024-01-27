85837a1ec66d058c594fb4ae55a0f457a4bbcd32
liz-fw <lstebbins@flowwest.com>
2024-01-26 16:59:07 -0800

    moves P2S environmental covarite comparison and exploration of results to data-raw

---
+++ data-raw/adult_model/explore_P2S_results.R
@@ -0,0 +1,53 @@
+# run passage to spawner
+library(SRJPEdata)
+library(SRJPEmodel)
+library(tidyverse)
+library(rstan)
+
+# pull in data from SRJPEdata ---------------------------------------------
+observed_adult_input <- SRJPEdata::observed_adult_input
+adult_model_covariates <- SRJPEdata::adult_model_covariates_standard
+
+
+battle_results <- run_passage_to_spawner_model(observed_adult_input,
+                                               adult_model_covariates,
+                                               "battle creek",
+                                               "wy_type")
+
+clear_results <- run_passage_to_spawner_model(observed_adult_input,
+                                              adult_model_covariates,
+                                              "clear creek",
+                                              "wy_type")
+
+deer_results <- run_passage_to_spawner_model(observed_adult_input,
+                                             adult_model_covariates,
+                                             "deer creek",
+                                             "wy_type")
+
+mill_results <- run_passage_to_spawner_model(observed_adult_input,
+                                             adult_model_covariates,
+                                             "mill creek",
+                                             "wy_type")
+
+# these are preliminary!
+butte_results <- run_passage_to_spawner_model(observed_adult_input,
+                                              adult_model_covariates,
+                                              "butte creek",
+                                              "gdd_std")
+
+
+# write model summaries ---------------------------------------------------
+P2S_model_fits <- bind_rows(battle_results$formatted_pars,
+                            clear_results$formatted_pars,
+                            mill_results$formatted_pars,
+                            deer_results$formatted_pars) |>
+  glimpse()
+
+
+# save to google cloud ----------------------------------------------------
+f <- function(input, output) write_csv(input, file = output)
+
+gcs_upload(P2S_model_fits,
+           object_function = f,
+           type = "csv",
+           name = "jpe-model-data/adult-model/P2S_model_fits.csv")
