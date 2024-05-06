78d697f88ff294c6ac7ee3fc073ff5d53d1af0ab
ergz <erodriguez@flowwest.com>
2024-05-06 15:50:18 -0700

    updates arguments passed to bt spas x to point to working winbugs directory

--- data-raw/explore-results.R
+++ data-raw/explore-results.R
@@ -1,17 +1,18 @@
 library(tidyverse)
 library(SRJPEdata)
 library(rstan)
+library(R2WinBUGS)
 
 # bt-spas-x ---------------------------------------------------------------
 # TODO test for different sites - put in stop if running on a site with no data in catch
 bt_spas_x_results <- run_bt_spas_x(SRJPEmodel::bt_spas_x_bayes_params,
                                    bt_spas_x_input_data = SRJPEdata::weekly_juvenile_abundance_model_data,
-                                   site = "deer creek",
-                                   run_year = 1994,
+                                   site = "ubc",
+                                   run_year = 2009,
                                    effort_adjust = T,
                                    multi_run_mode = F, # T
                                    mainstem_version = F,
-                                   bugs_directory = "not working")
+                                   bugs_directory = here::here("data-raw", "WinBUGS14"))
 
 
 # passage to spawner ------------------------------------------------------
