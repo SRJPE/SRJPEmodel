ad982cd02f9b5102ac1e14b706fd0ef4f7ea9277
ergz <erodriguez@flowwest.com>
2024-07-31 14:30:50 -0700

    adds pinning and board creation functions for azure

---
+++ R/storage.R
@@ -0,0 +1,81 @@
+
+#' @title Azure Blob Setup
+#' @description
+#' Creates an `AzureStor` storage container object to be used by the `pins` package.
+#'
+#' @param storage_account string storage account to create connection to
+#' @param access_key the access key that provides for the blob access. By default an env variable
+#' with the name `AZ_CONTAINER_ACCESS_KEY` is retrived, optionally you can directly pass in a value.
+#'
+#' @returns An `AzureStore` storage container object
+#' @export
+setup_azure_blob_backend <- function(storage_account, access_key=Sys.getenv("AZ_CONTAINER_ACCESS_KEY")) {
+  if (access_key == "") {
+    stop("access key is required in order to write and read from the azure boad", call. = FALSE)
+  }
+  storage_endpoint <- glue::glue("https://{storage_account}.blob.core.windows.net")
+  store <- AzureStor::storage_endpoint(storage_endpoint, key = access_key)
+  return(store)
+}
+
+#' @title Model Data Pin Board
+#' @description Creates a pin board for model data storage in Azure Blob Storage.
+#'
+#' @param storage_account The name of the Azure Storage account.
+#' @param container The name of the blob container within the storage account.
+#' @param ... Additional arguments passed to `setup_azure_blob_backend()`.
+#'
+#' @return A pins board object connected to the specified Azure Blob Storage container.
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' board <- model_pin_board("my_storage_account", "my_container")
+#' }
+#'
+#' @seealso
+#' \code{\link[AzureStor]{blob_container}}
+#' \code{\link[pins]{board_azure}}
+#' @export
+model_pin_board <- function(storage_account, container, ...) {
+  storage_client <- setup_azure_blob_backend(storage_account, ...)
+
+  all_containers <- AzureStor::list_blob_containers(storage_client)
+  container_names <- names(all_containers)
+
+  if (!(container %in% container_names)) {
+    stop(glue::glue("the blob '{container}' does not exist in your azure storage account"), call. = FALSE)
+  }
+
+  blob_client <- AzureStor::blob_container(storage_client, container)
+  blob_board <- pins::board_azure(container = blob_client, "model-fits/")
+
+  return(blob_board)
+}
+
+#' @title Model Data Pin
+#' @description
+#' Pin model data to provided pin board.
+#'
+#' @param board a board created with `model_pin_board`
+#' @param data data to pin
+#' @param name the name assigned to this data on blob, if wanting to add a new version of a previous result, re-use the name
+#' @param title title for data
+#' @param ... additional named arguments passed is as metadata to the pin
+#'
+#' @export
+pin_model_data <- function(board, data, name, title, description, ...) {
+  pin_metadata <- list(...)
+  pins::pin_write(board,
+                  data,
+                  name = name,
+                  title = title,
+                  description = description,
+                  metadata = pin_metadata
+  )
+}
+
+
+
+
