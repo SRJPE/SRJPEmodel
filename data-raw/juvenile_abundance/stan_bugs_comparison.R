bde65e92ba04ddb6f66a56a03609112227066e9e
Liz Stebbins <lstebbins@flowwest.com>
2024-10-28 10:40:57 -0600

    updates script to compare stan and bugs

--- data-raw/juvenile_abundance/stan_bugs_comparison.R
+++ data-raw/juvenile_abundance/stan_bugs_comparison.R
@@ -17,6 +17,9 @@ trials_to_fit <- weekly_catch |>
 
 nrow(trials_to_fit)
 
+trials_to_fit_battle <- trials_to_fit |>
+  filter(site %in% c("ubc", "lbc"))
+
 # function for running multiple -------------------------------------------
 
 run_multiple_bugs <- function(site, run_year, life_stage) {
@@ -43,6 +46,7 @@ run_multiple_stan <- function(site, run_year, life_stage) {
   cli::cli_bullets(paste("Running STAN on", site, "for run year", run_year, "and life stage", life_stage))
 
   # TODO do we want to add a function for processing these results?
+  # TODO we need to add something that binds in site, run_year, and life_stage
   results <- tryCatch({run_single_bt_spas_x_stan(SRJPEmodel::bt_spas_x_bayes_params,
                                                  weekly_catch,
                                                  weekly_efficiency,
@@ -51,6 +55,7 @@ run_multiple_stan <- function(site, run_year, life_stage) {
                                                  lifestage = life_stage,
                                                  effort_adjust = F)},
                       error = function(e) return("error")
+
   )
   return(results)
 }
@@ -61,30 +66,42 @@ run_multiple_stan <- function(site, run_year, life_stage) {
 bugs_results <- purrr::pmap(list(trials_to_fit$site,
                                  trials_to_fit$run_year,
                                  trials_to_fit$life_stage),
-                            run_multiple_bugs)
+                            run_multiple_bugs,
+                            .progress = TRUE)
 
 
 # run for STAN ------------------------------------------------------------
 
-stan_results <- purrr::pmap(list(trials_to_fit$site,
-                                 trials_to_fit$run_year,
-                                 trials_to_fit$life_stage),
-                            run_multiple_stan)
+options(mc.cores=parallel::detectCores())
+
+stan_results_battle <- purrr::pmap(list(trials_to_fit_battle$site,
+                                        trials_to_fit_battle$run_year,
+                                        trials_to_fit_battle$life_stage),
+                            run_multiple_stan,
+                            .progress = TRUE)
+saveRDS(stan_results_battle, here::here("data-raw", "juvenile_abundance",
+                                         "battle_results_STAN_oct_2024.rds"))
 
 
 # results -----------------------------------------------------------------
 
-# extract_results <- function(element) {
-#   if(!is.list(element)) {
-#     return(list("error" = TRUE))
-#   } else {
-#     return(element$final_results)
-#   }
-# }
-#
-# clean_results <- lapply(battle_results, extract_results) |>
-#   bind_rows() |>
-#   filter(error %in% c(FALSE, NA)) |>
-#   select(-error) |>
-#   glimpse()
+extract_stan_results <- function(element) {
+  par_list <- c("N", "pCap_U")
+
+  if(class(element) == "stanfit") {
+    results <- rstan::summary(element, pars = par_list)$summary |>
+      as.data.frame()
+    if(nrow(results) == 0) {
+      results <- data.frame("error" = TRUE)
+    }
+    return(results)
+  }
+}
+
+battle_stan_results_clean <- lapply(stan_results_battle,
+                                    extract_stan_results) |>
+  bind_rows()
+
+# bind in site and run year and lifestage
+
 
