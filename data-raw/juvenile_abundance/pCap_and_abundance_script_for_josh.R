32242180ea0956e053b1ddcba72fcac9b0cb56ea
liz-fw <lstebbins@flowwest.com>
2024-12-03 13:07:34 -0800

    builds out function to auto-run

--- data-raw/juvenile_abundance/pCap_and_abundance_script_for_josh.R
+++ data-raw/juvenile_abundance/pCap_and_abundance_script_for_josh.R
@@ -66,3 +66,53 @@ trials_to_fit <- SRJPEdata::weekly_juvenile_abundance_catch_data |>
   select(-n)
 
 
+# functionalize and run ---------------------------------------------------
+
+run_site_year <- function(site_arg, year_arg) {
+  cli::cli_bullets(paste0("Fitting for ", site_arg, " and run year ", year_arg))
+
+  inputs <- prepare_inputs_pCap_abundance_STAN(SRJPEdata::weekly_juvenile_abundance_catch_data,
+                                               SRJPEdata::weekly_juvenile_abundance_efficiency_data,
+                                               site = site_arg, run_year = year_arg, effort_adjust = T)
+
+  pCap <- fit_pCap_model(inputs$pCap_inputs)
+
+  # check outputs for convergence
+  check_pCap <- rstan::summary(pCap, pars = c("lt_pCap_U"))$summary |>
+    data.frame() |>
+    filter(Rhat > 1.05)
+
+  if(nrow(check_pCap) > 0) {
+    return(check_pCap)
+  }
+
+  # call abundance model - we need to pass in pCap to get the lt_pCap_U estimates
+  # and pass those in as data
+  abundance <- fit_abundance_model(inputs$abundance_inputs, pCap)
+
+  check_abundance <- bind_rows(rstan::summary(abundance, pars = c("lt_pCap_U"))$summary |>
+                                 data.frame() |>
+                                 filter(Rhat > 1.05),
+                               rstan::summary(abundance, pars = c("N"))$summary |>
+                                 data.frame() |>
+                                 filter(Rhat > 1.05))
+
+  if(nrow(check_abundance) > 0) {
+    return(check_abundance)
+  }
+
+  diagnostic_plots_split(site_arg, year_arg, abundance)
+
+  return(abundance)
+}
+
+trials_to_fit_lcc <- trials_to_fit |>
+  filter(site == "lcc")
+
+lcc <- purrr::pmap(list(trials_to_fit_lcc$site,
+                        trials_to_fit_lcc$run_year),
+                   run_site_year,
+                   .progress = T)
+
+
+
