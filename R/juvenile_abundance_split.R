b24554f2810becafb99044bd690b9300a85e6542
liz-fw <lstebbins@flowwest.com>
2024-12-19 14:30:13 -0800

    updates args for function for running abundance bugs model to allow you to point to your own directory

--- R/juvenile_abundance_split.R
+++ R/juvenile_abundance_split.R
@@ -494,6 +494,54 @@ generate_lt_pCap_Us <- function(abundance_inputs, pCap_model_object){
 }
 
 
+#' Run BUGS abundance model.
+#' @details This function runs the BUGS abundance model.
+#' @param abundance_inputs the object produced by `prepare_abundance_inputs()`
+#' @param lt_pCap_Us the object produced by `generate_lt_pCap_Us()`
+#' @param bugs_model_file the filepath pointing to where your BUGS abundance model file is
+#' @param bugs_directory the filepath pointing to where your WinBUGS14/ directory is
+#' @returns a BUGS object.
+#' @export
+#' @md
+fit_abundance_model_BUGS <- function(abundance_inputs, lt_pCap_Us,
+                                     bugs_model_file,
+                                     bugs_directory) {
+
+  parameters <- c("lt_pCap_U", "pCap_U", "N", "Ntot", "sd.N", "sd.Ne", "lg_CumN")
+  Nmcmc = 2000
+  Nburnin = 500
+  Nthin = 2
+  Nchains = 3
+
+  # clean up later
+  data <- abundance_inputs$inputs$data
+  data$lt_pCap_mu <- lt_pCap_Us$lt_pCap_mu
+  data$lt_pCap_tau <- 1/lt_pCap_Us$lt_pCap_sd^2
+
+  inits_with_lt_pCap_U <- abundance_inputs$inputs$inits[[1]]
+
+  inits_with_lt_pCap_U$lt_pCap_U <- data$lt_pCap_mu
+  inits_with_lt_pCap_U$tau.N <- 1
+  inits_with_lt_pCap_U$tau.Ne <- 1
+
+  new_inits <- list(inits_with_lt_pCap_U,
+                    inits_with_lt_pCap_U,
+                    inits_with_lt_pCap_U)
+
+  abundance <- bugs(data,
+                    new_inits,
+                    parameters,
+                    bugs_model_file, debug = F,
+                    n.chains = Nchains, n.burnin = Nburnin, n.thin = Nthin, n.iter = Nmcmc,
+                    codaPkg = F, DIC = T, clearWD = T,
+                    bugs.directory = bugs_directory)
+                    #bugs.directory = here::here("data-raw", "WinBUGS14"))
+
+  return(abundance)
+}
+
+
+
 #' Fit abundance model
 #' @details This function prepares the data list for the abundance STAN model based on what the model name is.
 #' @param input A list containing the inputs for the abundance model.
