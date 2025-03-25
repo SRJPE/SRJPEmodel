26221ad666219ab7819ff300412a64191623cba1
unknown <lstebbins@flowwest.com>
2025-03-25 14:33:02 -0700

    updates to read in most recent pcap fit

--- data-raw/juvenile_abundance/battle_fit_upload_test_script.R
+++ data-raw/juvenile_abundance/battle_fit_upload_test_script.R
@@ -1,10 +1,17 @@
+library(tidyverse)
+library(DBI)
+library(SRJPEdata)
+library(R2WinBUGS)
+library(AzureStor)
+library(RPostgres)
+
 # fit all the battle creek abundance models and push to cloud
 battle_run_years <- SRJPEdata::weekly_juvenile_abundance_catch_data |>
   filter(site == "ubc") |>
   distinct(run_year) |>
   pull(run_year)
 
-pcap_object <- readRDS("~/Downloads/pCap_model_2025-02-10.rds")
+pcap_object <- readRDS("C:/Users/Liz/Downloads/pCap_model_2025-03-25.rds")
 
 # set up db connection
 cfg <- config::get()
@@ -20,11 +27,13 @@ con <- DBI::dbConnect(RPostgres::Postgres(),
 for(i in battle_run_years) {
   inputs <- prepare_abundance_inputs("ubc", i, effort_adjust = T)
   lt_pcap_us <- generate_lt_pCap_Us(inputs, pcap_object)
+  print(paste("Fitting model for", i))
   fit <- fit_abundance_model_BUGS(inputs, lt_pcap_us,
                                   "C:/Users/Liz/Documents/SRJPEmodel/model_files/abundance_model.bug",
                                   "C:/Users/Liz/Documents/SRJPEmodel/data-raw/WinBUGS14")
 
   # push to cloud
+  print(paste("Uploading to cloud for", i))
   store_model_fit(con,
                   storage_account = "jpemodelresults",
                   container_name = "model-results",
@@ -33,7 +42,7 @@ for(i in battle_run_years) {
                   results_name = "juvenile_abundance",
                   site = "ubc",
                   run_year = i,
-                  description = paste("ubc", i, "model fit object"))
+                  description = paste("ubc", i, "model fit object from auto-run tests"))
 
 }
 
